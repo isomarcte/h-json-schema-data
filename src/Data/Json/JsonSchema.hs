f7e31f4936e5388c132c1c827a06e88f3f286d50
David Strawn <isomarcte@gmail.com>
2019-03-07 07:57:22 -0700

    squash!

--- src/Data/Json/JsonSchema.hs
+++ src/Data/Json/JsonSchema.hs
@@ -1,18 +1,23 @@
-module Data.Json.JsonSchema (JsonSchema(..)
-                            , JsonBooleanSchema(..)
-                            , JsonObjectSchema(..)
-                            , TypeKey(..)
-                            , OneOrSome(..)
-                            , emptyJsonObjectSchema
-                            ) where
+module Data.Json.JsonSchema
+  ( JsonSchema(..)
+  , JsonBooleanSchema(..)
+  , JsonObjectSchema(..)
+  , TypeKey(..)
+  , OneOrSome(..)
+  , emptyJsonObjectSchema
+  , objectSchema
+  , objectSchema'
+  ) where
 
+import Control.Applicative (Applicative(..), (<$>))
 import Data.Bool (Bool(..))
 import Data.Eq (Eq)
-import Data.Function (($), (.), id)
+import Data.Function (($), (.))
 import Data.Functor (fmap)
+import Data.Functor.Identity (Identity(..))
 import Data.Int (Int)
 import Data.List.NonEmpty (NonEmpty(..), toList)
-import Data.Maybe (Maybe(..), maybe)
+import Data.Maybe (Maybe(..), fromMaybe)
 import Data.Monoid ((<>))
 import Data.Text as DT
 import Data.Tuple (swap)
@@ -28,8 +33,22 @@ data JsonSchema where
   BooleanSchema :: JsonBooleanSchema -> JsonSchema
   ObjectSchema :: JsonObjectSchema -> JsonSchema
 
+objectSchema ::
+     (Applicative f)
+  => (JsonObjectSchema -> f JsonObjectSchema)
+  -> JsonSchema
+  -> f JsonSchema
+objectSchema f (ObjectSchema j) = ObjectSchema <$> f j
+objectSchema _ j = pure j
+
+objectSchema' ::
+     (JsonObjectSchema -> JsonObjectSchema) -> JsonSchema -> JsonSchema
+objectSchema' f jos = runIdentity $ objectSchema (pure . f) jos
+
 deriving instance Show JsonSchema
+
 deriving instance Eq JsonSchema
+
 deriving instance Generic JsonSchema
 
 instance DA.FromJSON JsonSchema where
@@ -38,36 +57,41 @@ instance DA.FromJSON JsonSchema where
 instance DA.ToJSON JsonSchema where
   toJSON = DA.genericToJSON untaggedJsonOptions
 
-newtype JsonBooleanSchema = JsonBooleanSchema Bool deriving (Show, Eq, DA.ToJSON, DA.FromJSON)
+newtype JsonBooleanSchema =
+  JsonBooleanSchema Bool
+  deriving (Show, Eq, DA.ToJSON, DA.FromJSON)
 
 data JsonObjectSchema where
-  JsonObjectSchema :: { schemaRef :: Maybe DT.Text
-                      , idRef :: Maybe DT.Text
-                      , typeKey :: Maybe TypeKey
-                      , enumKey :: Maybe [DA.Value]
-                      , constKey :: Maybe DA.Value
-                      , multipleOfKey :: Maybe Word
-                      , maximumKey :: Maybe Int
-                      , exclusiveMaximumKey :: Maybe Int
-                      , minimumKey :: Maybe Int
-                      , exclusiveMinimumKey :: Maybe Int
-                      , maxLengthKey :: Maybe Word
-                      , minLengthKey :: Maybe Word
-                      , patternKey :: Maybe DT.Text
-                      , itemsKey :: Maybe ItemsKey
-                      , additionalItemsKey :: Maybe AdditionalItemsKey
-                      , maxItemsKey :: Maybe Word
-                      , minItemsKey :: Maybe Word
-                      , uniqueItemsKey :: Maybe Bool
-                      , containsKey :: Maybe JsonSchema
-                      , maxPropertiesKey :: Maybe Word
-                      , minPropertiesKey :: Maybe Word
-                      , requiredKey :: Maybe [DT.Text]
-                      , propertiesKey :: Maybe (DMS.Map DT.Text JsonSchema)
-                      } -> JsonObjectSchema
+  JsonObjectSchema
+    :: { schemaRef :: Maybe DT.Text
+       , idRef :: Maybe DT.Text
+       , typeKey :: Maybe TypeKey
+       , enumKey :: Maybe [DA.Value]
+       , constKey :: Maybe DA.Value
+       , multipleOfKey :: Maybe Word
+       , maximumKey :: Maybe Int
+       , exclusiveMaximumKey :: Maybe Int
+       , minimumKey :: Maybe Int
+       , exclusiveMinimumKey :: Maybe Int
+       , maxLengthKey :: Maybe Word
+       , minLengthKey :: Maybe Word
+       , patternKey :: Maybe DT.Text
+       , itemsKey :: Maybe ItemsKey
+       , additionalItemsKey :: Maybe AdditionalItemsKey
+       , maxItemsKey :: Maybe Word
+       , minItemsKey :: Maybe Word
+       , uniqueItemsKey :: Maybe Bool
+       , containsKey :: Maybe JsonSchema
+       , maxPropertiesKey :: Maybe Word
+       , minPropertiesKey :: Maybe Word
+       , requiredKey :: Maybe [DT.Text]
+       , propertiesKey :: Maybe (DMS.Map DT.Text JsonSchema)}
+    -> JsonObjectSchema
 
 deriving instance Eq JsonObjectSchema
+
 deriving instance Generic JsonObjectSchema
+
 deriving instance Show JsonObjectSchema
 
 instance DA.FromJSON JsonObjectSchema where
@@ -77,37 +101,41 @@ instance DA.ToJSON JsonObjectSchema where
   toJSON = toJsonKeyMangle . DA.genericToJSON genericJsonOptions
 
 emptyJsonObjectSchema :: JsonObjectSchema
-emptyJsonObjectSchema = JsonObjectSchema { schemaRef = Nothing
-                                         , idRef = Nothing
-                                         , typeKey = Nothing
-                                         , enumKey = Nothing
-                                         , constKey = Nothing
-                                         , multipleOfKey = Nothing
-                                         , maximumKey = Nothing
-                                         , exclusiveMaximumKey = Nothing
-                                         , minimumKey = Nothing
-                                         , exclusiveMinimumKey = Nothing
-                                         , maxLengthKey = Nothing
-                                         , minLengthKey = Nothing
-                                         , patternKey = Nothing
-                                         , itemsKey = Nothing
-                                         , additionalItemsKey = Nothing
-                                         , maxItemsKey = Nothing
-                                         , minItemsKey = Nothing
-                                         , uniqueItemsKey = Nothing
-                                         , containsKey = Nothing
-                                         , maxPropertiesKey = Nothing
-                                         , minPropertiesKey = Nothing
-                                         , requiredKey = Nothing
-                                         , propertiesKey = Nothing
-                                         }
+emptyJsonObjectSchema =
+  JsonObjectSchema
+    { schemaRef = Nothing
+    , idRef = Nothing
+    , typeKey = Nothing
+    , enumKey = Nothing
+    , constKey = Nothing
+    , multipleOfKey = Nothing
+    , maximumKey = Nothing
+    , exclusiveMaximumKey = Nothing
+    , minimumKey = Nothing
+    , exclusiveMinimumKey = Nothing
+    , maxLengthKey = Nothing
+    , minLengthKey = Nothing
+    , patternKey = Nothing
+    , itemsKey = Nothing
+    , additionalItemsKey = Nothing
+    , maxItemsKey = Nothing
+    , minItemsKey = Nothing
+    , uniqueItemsKey = Nothing
+    , containsKey = Nothing
+    , maxPropertiesKey = Nothing
+    , minPropertiesKey = Nothing
+    , requiredKey = Nothing
+    , propertiesKey = Nothing
+    }
 
 data OneOrSome a where
   One :: a -> OneOrSome a
   Some :: [a] -> OneOrSome a
 
 deriving instance Show a => Show (OneOrSome a)
+
 deriving instance Eq a => Eq (OneOrSome a)
+
 deriving instance Generic (OneOrSome a)
 
 instance DA.FromJSON a => DA.FromJSON (OneOrSome a) where
@@ -116,28 +144,55 @@ instance DA.FromJSON a => DA.FromJSON (OneOrSome a) where
 instance DA.ToJSON a => DA.ToJSON (OneOrSome a) where
   toJSON = DA.genericToJSON untaggedJsonOptions
 
-newtype ItemsKey = ItemsKey (OneOrSome JsonSchema) deriving (Show, Eq, DA.ToJSON, DA.FromJSON)
-newtype AdditionalItemsKey = AdditionalItemsKey (OneOrSome JsonSchema) deriving (Show, Eq, DA.ToJSON, DA.FromJSON)
-newtype TypeKey = TypeKey (OneOrSome DT.Text) deriving (Show, Eq, DA.ToJSON, DA.FromJSON)
+newtype ItemsKey =
+  ItemsKey (OneOrSome JsonSchema)
+  deriving (Show, Eq, DA.ToJSON, DA.FromJSON)
+
+newtype AdditionalItemsKey =
+  AdditionalItemsKey (OneOrSome JsonSchema)
+  deriving (Show, Eq, DA.ToJSON, DA.FromJSON)
+
+newtype TypeKey =
+  TypeKey (OneOrSome DT.Text)
+  deriving (Show, Eq, DA.ToJSON, DA.FromJSON)
 
 genericJsonOptions :: DA.Options
-genericJsonOptions = DA.defaultOptions { DA.unwrapUnaryRecords = True }
+genericJsonOptions = DA.defaultOptions {DA.unwrapUnaryRecords = True}
 
 untaggedJsonOptions :: DA.Options
 untaggedJsonOptions = genericJsonOptions {DA.sumEncoding = DA.UntaggedValue}
 
 keyRemappingsL :: NonEmpty (DT.Text, DT.Text)
-keyRemappingsL =
-  ("$schema", "schemaRef") :| ("$id", "idRef"):keyList
+keyRemappingsL = ("$schema", "schemaRef") :| ("$id", "idRef") : keyList
   where
     toTuple :: DT.Text -> (DT.Text, DT.Text)
     toTuple k = (k, k <> "Key")
     keyList :: [(DT.Text, DT.Text)]
-    keyList = fmap toTuple ["enum", "const", "multipleOf", "maximum", "exclusiveMaximum",
-                            "minimum", "exclusiveMinimum", "maxLength", "minLength", "pattern",
-                            "type", "items", "additionalItems", "maxItems", "minItems", "uniqueItems",
-                            "contains", "maxProperties", "minProperties", "required", "properties"
-                           ]
+    keyList =
+      fmap
+        toTuple
+        [ "enum"
+        , "const"
+        , "multipleOf"
+        , "maximum"
+        , "exclusiveMaximum"
+        , "minimum"
+        , "exclusiveMinimum"
+        , "maxLength"
+        , "minLength"
+        , "pattern"
+        , "type"
+        , "items"
+        , "additionalItems"
+        , "maxItems"
+        , "minItems"
+        , "uniqueItems"
+        , "contains"
+        , "maxProperties"
+        , "minProperties"
+        , "required"
+        , "properties"
+        ]
 
 fromKeyRemappings :: DMS.Map DT.Text DT.Text
 fromKeyRemappings = DMS.fromList . toList $ keyRemappingsL
@@ -146,12 +201,17 @@ toKeyRemappings :: DMS.Map DT.Text DT.Text
 toKeyRemappings = DMS.fromList . toList $ fmap swap keyRemappingsL
 
 keyMangle :: DMS.Map DT.Text DT.Text -> DT.Text -> DT.Text
-keyMangle m t = maybe t id $ DMS.lookup t m
+keyMangle m t = fromMaybe t $ DMS.lookup t m
 
 jsonKeyMangle :: DMS.Map DT.Text DT.Text -> DA.Value -> DA.Value
-jsonKeyMangle m (DA.Object hm) = DA.Object $ DHS.foldlWithKey' (f m) DHS.empty hm
+jsonKeyMangle m (DA.Object hm) =
+  DA.Object $ DHS.foldlWithKey' (f m) DHS.empty hm
   where
-    f :: DMS.Map DT.Text DT.Text -> DHS.HashMap DT.Text DA.Value -> DT.Text -> DA.Value -> DHS.HashMap DT.Text DA.Value
+    f :: DMS.Map DT.Text DT.Text
+      -> DHS.HashMap DT.Text DA.Value
+      -> DT.Text
+      -> DA.Value
+      -> DHS.HashMap DT.Text DA.Value
     f m' acc k v = DHS.insert (keyMangle m' k) v acc
 jsonKeyMangle _ v = v
 
@@ -160,9 +220,7 @@ toJsonKeyMangle = jsonKeyMangle toKeyRemappings
 
 fromJsonKeyMangle :: DA.Value -> DA.Value
 fromJsonKeyMangle = jsonKeyMangle fromKeyRemappings
-
 -- data TypeEnumeration = TypeInteger | TypeNull | TypeBoolean | TypeObject | TypeString | TypeNumber | TypeArray deriving (Eq, Ord)
-
 -- instance Show TypeEnumeration where
 --   show TypeNull = "null"
 --   show TypeBoolean = "boolean"
@@ -171,7 +229,6 @@ fromJsonKeyMangle = jsonKeyMangle fromKeyRemappings
 --   show TypeNumber = "number"
 --   show TypeArray = "array"
 --   show TypeInteger = "integer"
-
 -- instance DA.FromJSON TypeEnumeration where
 --   parseJSON (DA.String "null") = pure TypeNull
 --   parseJSON (DA.String "boolean") = pure TypeBoolean
@@ -181,6 +238,5 @@ fromJsonKeyMangle = jsonKeyMangle fromKeyRemappings
 --   parseJSON (DA.String "array") = pure TypeArray
 --   parseJSON (DA.String "integer") = pure TypeInteger
 --   parseJSON v = DAT.typeMismatch "String" v
-
 -- instance DA.ToJSON TypeEnumeration where
 --   toJSON = DA.String . DT.pack . show
--- test/Unit/ExampleSchemas.hs
+++ test/Unit/ExampleSchemas.hs
@@ -1,6 +1,7 @@
-module Unit.ExampleSchemas (TestComparison(..)
-                           , schemas
-                           ) where
+module Unit.ExampleSchemas
+  ( TestComparison(..)
+  , schemas
+  ) where
 
 import Control.Applicative (Applicative(..))
 import Control.Monad (foldM, return)
@@ -9,15 +10,15 @@ import Data.Eq (Eq)
 import Data.Function (($), (.), id)
 import Data.Functor (fmap)
 import Data.List.NonEmpty (NonEmpty(..))
+import Data.Maybe (Maybe(..))
 import Data.Monoid ((<>))
 import Data.Ord (Ord)
 import Data.String (String)
 import Data.Traversable (traverse)
 import GHC.Err (error)
 import System.FilePath.Posix (takeFileName)
-import System.IO (IO, FilePath)
+import System.IO (FilePath, IO)
 import Text.Show (Show(..))
-import Data.Maybe (Maybe(..))
 
 import qualified Data.Aeson as DA
 import qualified Data.Json.JsonSchema as DJJ
@@ -25,42 +26,71 @@ import qualified Data.Map.Strict as DMS
 import qualified Data.Text as DT
 import qualified Paths_h_json_schema_data as Paths
 
-data TestComparison a = TestComparison {actual :: a, expected :: a} deriving (Show, Eq)
+data TestComparison a = TestComparison
+  { actual :: a
+  , expected :: a
+  } deriving (Show, Eq)
 
 schemaFilePaths :: IO (NonEmpty FilePath)
-schemaFilePaths = traverse Paths.getDataFileName $ fmap ("data/test/example/" <>) ["schema0.json"]
+schemaFilePaths =
+  traverse Paths.getDataFileName $
+  fmap ("data/test/example/" <>) ["schema0.json"]
 
 schemaFileMap :: IO (DMS.Map FilePath DJJ.JsonSchema)
 schemaFileMap = do
   sfps <- schemaFilePaths
   foldM f DMS.empty sfps
   where
-    f :: DMS.Map FilePath DJJ.JsonSchema -> FilePath -> IO (DMS.Map FilePath DJJ.JsonSchema)
+    f :: DMS.Map FilePath DJJ.JsonSchema
+      -> FilePath
+      -> IO (DMS.Map FilePath DJJ.JsonSchema)
     f acc value = do
-      mjs <- DA.eitherDecodeFileStrict' value :: IO (Either String DJJ.JsonSchema)
-      let js = either (\e -> error $ "Unable to decode JsonSchema in file: " <> value <> "\nError: " <> e) id mjs
+      mjs <-
+        DA.eitherDecodeFileStrict' value :: IO (Either String DJJ.JsonSchema)
+      let js =
+            either
+              (\e ->
+                 error $
+                 "Unable to decode JsonSchema in file: " <> value <> "\nError: " <>
+                 e)
+              id
+              mjs
       return $ DMS.insert (takeFileName value) js acc
 
 lookupOrError :: (Ord a, Show a) => a -> DMS.Map a b -> b
 lookupOrError a m =
   case DMS.lookup a m of
     Just v -> v
-    _ -> error $ "Unable to find " <> show a <> " in map\nMap Keys" <> show (DMS.keysSet m)
+    _ ->
+      error $
+      "Unable to find " <> show a <> " in map\nMap Keys" <> show (DMS.keysSet m)
+
+emptyWithTypeKey :: DT.Text -> DJJ.JsonSchema
+emptyWithTypeKey t = DJJ.ObjectSchema $ DJJ.emptyJsonObjectSchema {DJJ.typeKey = pure . DJJ.TypeKey $ DJJ.One t}
 
 schemas :: IO (NonEmpty (TestComparison DJJ.JsonSchema))
-schemas =
-  fmap f schemaFileMap
+schemas = fmap f schemaFileMap
   where
-    f :: DMS.Map FilePath DJJ.JsonSchema -> NonEmpty (TestComparison DJJ.JsonSchema)
+    f :: DMS.Map FilePath DJJ.JsonSchema
+      -> NonEmpty (TestComparison DJJ.JsonSchema)
     f m =
-      TestComparison {actual = lookupOrError "schema0.json" m
-                     , expected = DJJ.ObjectSchema $ DJJ.emptyJsonObjectSchema  { DJJ.schemaRef = pure jsonSchemaDraft07SchemaRef
-                                            , DJJ.idRef  = pure "http://example.com/product.schema.json"
-                                            , DJJ.typeKey = pure . DJJ.TypeKey $ DJJ.One "object"
-                                            , DJJ.propertiesKey = pure . DMS.fromList $ [("productId"
-                                                                                         , DJJ.ObjectSchema $ DJJ.emptyJsonObjectSchema {DJJ.typeKey = pure . DJJ.TypeKey . DJJ.One $ "integer"})]
-                                            }
-                     } :| []
+      TestComparison
+        { actual = lookupOrError "schema0.json" m
+        , expected =
+            DJJ.ObjectSchema $
+            DJJ.emptyJsonObjectSchema
+              { DJJ.schemaRef = pure jsonSchemaDraft07SchemaRef
+              , DJJ.idRef = pure "http://example.com/product.schema.json"
+              , DJJ.typeKey = pure . DJJ.TypeKey $ DJJ.One "object"
+              , DJJ.propertiesKey =
+                  pure . DMS.fromList $
+                  [ ( "productId" , emptyWithTypeKey "integer")
+                  , ("productname", emptyWithTypeKey "string")
+                  , ("price", DJJ.objectSchema' (\jos -> jos {DJJ.exclusiveMinimumKey = pure 0}) $ emptyWithTypeKey "number")
+                  ]
+              }
+        } :|
+      []
 
 jsonSchemaDraft07SchemaRef :: DT.Text
 jsonSchemaDraft07SchemaRef = "http://json-schema.org/draft-07/schema#"
